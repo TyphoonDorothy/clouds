ff5e50662cf988ca46780be975d6a5df67f88056
TyphoonDorothy <odarkaz66@gmail.com>
2025-09-22 19:37:21 +0300

    pls work

--- lab5/app/my_project/sportsman/routes/coach_routes.py
+++ lab5/app/my_project/sportsman/routes/coach_routes.py
@@ -1,3 +1,4 @@
+import os
 from flask import Blueprint, jsonify, request
 from sqlalchemy import text
 from ..controller.orders.coach_controller import CoachController
@@ -7,38 +8,38 @@ from flasgger import swag_from
 coach_bp = Blueprint("coach", __name__)
 coach_controller = CoachController()
 
+# Absolute path to the YAML file
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SWAGGER_FILE = os.path.join(BASE_DIR, '../swagger/coach.yaml')
+
 @coach_bp.route("/coach", methods=['GET'])
-@swag_from('../swagger/coach.yaml', methods=['GET'])
+@swag_from(SWAGGER_FILE, methods=['GET'])
 def get_coach():
     return coach_controller.get_all()
 
 @coach_bp.route("/coach/<int:coach_id>", methods=['GET'])
-@swag_from('../swagger/coach.yaml', methods=['GET'])
+@swag_from(SWAGGER_FILE, methods=['GET'])
 def get_coach_by_id(coach_id):
     return coach_controller.get_by_id(coach_id)
 
 @coach_bp.route("/coach", methods=['POST'])
-@swag_from('../swagger/coach.yaml', methods=['POST'])
+@swag_from(SWAGGER_FILE, methods=['POST'])
 def add_coach():
     data = request.get_json()
-    # use data["id"], data["name"], etc.
     return {"message": "Coach added"}, 200
 
-# def add_coach():
-#     return coach_controller.create()
-
 @coach_bp.route("/coach/<int:coach_id>", methods=['PATCH'])
-@swag_from('../swagger/coach.yaml', methods=['PATCH'])
+@swag_from(SWAGGER_FILE, methods=['PATCH'])
 def update_coach(coach_id):
     return coach_controller.update(coach_id)
 
 @coach_bp.route("/coach/<int:coach_id>", methods=['DELETE'])
-@swag_from('../swagger/coach.yaml', methods=['DELETE'])
+@swag_from(SWAGGER_FILE, methods=['DELETE'])
 def delete_coach(coach_id):
     return coach_controller.delete(coach_id)
 
 @coach_bp.route("/coach/insert", methods=['POST'])
-@swag_from('../swagger/coach.yaml', methods=['POST'])
+@swag_from(SWAGGER_FILE, methods=['POST'])
 def insert_coach():
     try:
         data = request.get_json()
@@ -51,15 +52,8 @@ def insert_coach():
             return jsonify({"error": "All fields are required."}), 400
 
         db.session.execute(
-            text("""
-                CALL insert_into_coach(:name, :surname, :specialization_id, :contact_id)
-            """),
-            {
-                "name": name,
-                "surname": surname,
-                "specialization_id": specialization_id,
-                "contact_id": contact_id,
-            }
+            text("""CALL insert_into_coach(:name, :surname, :specialization_id, :contact_id)"""),
+            {"name": name, "surname": surname, "specialization_id": specialization_id, "contact_id": contact_id}
         )
         db.session.commit()
 
@@ -67,12 +61,3 @@ def insert_coach():
     except Exception as e:
         db.session.rollback()
         return jsonify({"error": str(e)}), 400
-
-
-@coach_bp.route("/dbping")
-def dbping():
-    try:
-        result = db.session.execute("SELECT 1").fetchone()
-        return {"ok": result[0]}
-    except Exception as e:
-        return {"error": str(e)}, 500
