bdbb053f020b950ff9a9c8ab8c9cd9b5386d010d
TyphoonDorothy <odarkaz66@gmail.com>
2025-09-23 21:38:20 +0300

    doctor

--- lab5/app/my_project/sportsman/domain/doctor.py
+++ lab5/app/my_project/sportsman/domain/doctor.py
@@ -9,8 +9,9 @@ class Doctor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(45), nullable=False)
     surname = db.Column(db.String(45), nullable=False)
-    contact_id = db.Column("doctors_contact_id", db.Integer, ForeignKey('doctors_contact.id'), ...)
-    specialization_id = db.Column("doctor_specialization_id", db.Integer, ForeignKey('doctor_specialization.id'), ...)
+    doctor_specialization_id = db.Column('doctor_specialization_id', db.Integer, ForeignKey('doctor_specialization.id'), nullable=False)
+    doctor_contact_id = db.Column('doctors_contact_id', db.Integer, ForeignKey('doctors_contact.id'), unique=True, nullable=False)
+
 
     specialization = db.relationship('DoctorSpecialization', back_populates= 'doctors')
     contact = db.relationship('DoctorsContact', back_populates= 'doctors')
