7cb019e183c65f9fae813e8d9895a88876627e7f
TyphoonDorothy <odarkaz66@gmail.com>
2025-09-25 19:16:59 +0300

    added basic auth #2

--- lab5/app/wsgi.py
+++ lab5/app/wsgi.py
@@ -28,4 +28,4 @@ def index():
     return "Hello, you are authenticated!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
